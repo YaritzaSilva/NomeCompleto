352c93bddeabdd2d5e6d4caaf39c7f4f5e8fdc57
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-08 18:28:15 -0300

    tarefa NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            string NomeCompleto;
+            string NomeCatalogo;
+
+            Console.WriteLine ("----------");
+            Console.WriteLine ("Veja seu nome em diferentes perspectivas");
+            Console.WriteLine ("----------\n\n");
+
+            Console.Write("Digite seu primeiro nome: ");
+            nome= Console.ReadLine();
+
+            Console.Write("Digite seu sobrenome: ");
+            sobrenome= Console.ReadLine();
+
+            NomeCompleto = $"{nome} {sobrenome}";
+            NomeCatalogo = $"{sobrenome.ToUpper()},{nome}";
+
+            Console.WriteLine();
+            Console.WriteLine($"Nome completo: {NomeCompleto}");
+            Console.WriteLine($"Nome de catálogo: {NomeCatalogo}");
+        }
+    }
+}
